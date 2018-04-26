e3248efed071ab56e8864e7277703d5612881196
Richard Vanegas <richardvanegas8@hotmail.com>
2018-04-26 12:02:04 -0500

    Primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,67 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  commentsBayern = 2585;
+  commentsJuan = 7;
+
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
 
+
+  commentPhoto(lugar:string) {
+    let prompt = this.alertCtrl.create({
+      title: 'Comentar foto',
+      message: "Ingresa tu comentario",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: '...'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Comentar',
+          handler: data => {
+            if(lugar=='juan'){
+              this.commentsJuan++;
+            }else{
+              this.commentsBayern++;
+            }
+            console.log('Saved clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+  likes() {
+    let alert = this.alertCtrl.create({
+      title: 'Tiene 12 Me gusta',
+      subTitle: 'Tu foto tiene 12 me gusta',
+      buttons: ['OK']
+    });
+    alert.present();
+
+
+  }
+
+
+  download() {
+    let alert = this.alertCtrl.create({
+      title: 'Download',
+      subTitle: 'Se ha descargado la foto',
+      buttons: ['OK']
+    });
+    alert.present();
+
+    
+  }
 }
